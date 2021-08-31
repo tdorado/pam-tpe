01d04d74d3e1ff6ede8a86fb26d732db8984b85f
Tomás Dorado <tdorado@itba.edu.ar>
2021-08-31 12:16:32 -0300

    added user with login logic to backend (#16)

---
+++ wallendar-backend/src/main/java/com/td/wallendarbackend/configurations/SecurityConfiguration.java
@@ -0,0 +1,59 @@
+package com.td.wallendarbackend.configurations;
+
+import com.td.wallendarbackend.security.AuthenticationFilter;
+import com.td.wallendarbackend.security.AuthorizationFilter;
+import com.td.wallendarbackend.services.ApplicationUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static com.td.wallendarbackend.constants.SecurityConstants.SIGN_UP_URL;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final ApplicationUserDetailsService userDetailsService;
+
+    public SecurityConfiguration(ApplicationUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new AuthenticationFilter(authenticationManager()))
+                .addFilter(new AuthorizationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
+}
---
+++ wallendar-backend/src/main/java/com/td/wallendarbackend/constants/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.td.wallendarbackend.constants;
+
+public class SecurityConstants {
+    public static final String SIGN_UP_URL = "/users/create";
+    public static final String KEY = "ThWmZq3t6w9z$C&F)J@NcRfUjXn2r5u7x!A%D*G-KaPdSgVkYp3s6v9y/B?E(H+M";
+    public static final String HEADER_NAME = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final Long EXPIRATION_TIME = 1000L*60*30;
+}
