addf8a7c27ebb40ff8ce5add9b0fe1a1eea0678d
tdallas <tdallas@itba.edu.ar>
2021-09-09 16:08:45 -0300

    feat: start mocking data

--- mock-backend/app.js
+++ mock-backend/app.js
@@ -1,39 +1,44 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const router = express.Router();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
-router.post('/debt/pay', (req, res) => {});
+router.post("/debt/pay", (req, res) => {
+  const { debt_id, from_user_id, to_user_id, amount } = req;
+  return res.json({ amount_payed: amount });
+});
 
-router.get('/groups/{userId}', (req, res) => {});
+router.get("/groups/{userId}", (req, res) => {
+    
+});
 
-router.get('/groups/{groupId}', (req, res) => {});
+router.get("/groups/{groupId}", (req, res) => {});
 
-router.get('/events/{userId}', (req, res) => {});
+router.get("/events/{userId}", (req, res) => {});
 
-router.get('/events/{eventId}', (req, res) => {});
+router.get("/events/{eventId}", (req, res) => {});
 
 // to event or group
-router.post('/charge', (req, res) => {})
+router.post("/charge", (req, res) => {});
 
-app.use('/', router);
+app.use("/", router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
+  // set locals, only providing error in development
+  res.locals.message = err.message;
 
-    // render the error page
-    res.status(err.status || 500);
-    res.json('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.json("error");
 });
 
 app.listen(9090);
