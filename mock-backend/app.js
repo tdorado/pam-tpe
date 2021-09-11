e8e2474d4a2df5ffc031b994a4cc1c980417b94a
tdallas <tdallas@itba.edu.ar>
2021-09-11 19:38:45 -0300

    feat: added mock backend in express (#32)
    * feat: added mock backend in express
    
    * feat: start mocking data
    
    * feat: added mock jsons
    
    * feat: mock products and events
    
    * feat: added full backend mocked
    
    * feat: added curls

---
+++ mock-backend/app.js
@@ -0,0 +1,86 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json())
+
+const router = express.Router();
+
+const { groups } = require("./groups.json");
+const { events } = require("./events.json");
+const { users } = require("./users.json");
+console.log("loaded jsons")
+let chargeIds = 1;
+
+router.post("/debt/pay", (req, res) => {
+  console.log("Received request to", req.path, "with params", req.body);
+  const { debt_id, from_user_id, to_user_id, amount } = req.body;
+  return res.json({ amount_payed: amount });
+});
+
+router.get("/users", (req, res) => {
+  console.log("Received request to", req.path, "with params", req.query);
+  const { user_email } = req.query;
+  if (user_email === "tdallas@itba.edu.ar") {
+    res.json({ user: users[0] });
+  } else if (userEmail === "tdorado@itba.edu.ar") {
+    res.json({ user: users[1] });
+  } else {
+    res.json({ user: users[2] });
+  }
+});
+
+router.get("/groups", (req, res) => {
+  console.log("Received request to", req.path, "with params", req.query);
+
+  res.json({ groups });
+});
+
+router.get("/groups/:group_id", (req, res) => {
+  console.log("Received request to", req.path, "with params", req.params);
+
+  const { group_id } = req.params;
+  res.json({ group: groups[group_id] });
+});
+
+router.get("/events", (req, res) => {
+  console.log("Received request to", req.path, "with params", req.query);
+
+  res.json({ events });
+});
+
+router.get("/events/:event_id", (req, res) => {
+  console.log("Received request to", req.path, "with params", req.params);
+
+  const { event_id } = req.params;
+  res.json({ event: events[event_id] });
+});
+
+// to event or group
+router.post("/charge", (req, res) => {
+  console.log("Received request to", req.path, "with params", req.body);
+
+  res.json({ charge: { id: chargeIds++, ...req.body } });
+});
+
+app.use("/", router);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json("error");
+});
+
+console.log("Listening pot 8080")
+app.listen(8080);
+
+module.exports = app;
---
+++ mock-backend/curls.sh
@@ -0,0 +1,37 @@
+curl --request POST \
+  --url http://localhost:8080/debt/pay \
+  --header 'Content-Type: application/json' \
+  --data '{
+	"debt_id": 2,
+	"from_user_id": 1,
+	"to_user_id": 2,
+	"amount": 1252.3
+}'
+
+curl --request GET \
+  --url 'http://localhost:8080/users?user_email=tdallas%40itba.edu.ar'
+
+  curl --request GET \
+  --url 'http://localhost:8080/groups?user_id=2'
+
+
+  curl --request GET \
+  --url http://localhost:8080/groups/1
+
+  curl --request GET \
+  --url 'http://localhost:8080/events?user_id=2'
+
+  curl --request GET \
+  --url http://localhost:8080/events/1
+
+  curl --request POST \
+  --url http://localhost:8080/charge \
+  --header 'Content-Type: application/json' \
+  --data '{
+		"owner": "tdallas@itba.edu.ar",
+		"title": "Cocas",
+		"chargeType": "EQUALLY",
+		"debtors": ["tdorado@itba.edu.ar"],
+		"amount": 250,
+		"date": "2021-09-11T15:36:54.606Z"
+}'
