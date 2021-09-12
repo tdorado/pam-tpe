076d993a4f0e57a36c58dc345eb2b8f58252e31b
tdallas <tdallas@itba.edu.ar>
2021-09-12 13:05:32 -0300

    feat: implement dto calsses

--- wallendar-app/app/src/main/java/com/td/wallendar/dtos/request/PayDebtRequest.java
+++ wallendar-app/app/src/main/java/com/td/wallendar/dtos/request/PayDebtRequest.java
@@ -1,4 +1,40 @@
 package com.td.wallendar.dtos.request;
 
 public class PayDebtRequest {
+    private Long fromUserId;
+    private Long toUserId;
+    private double amount;
+
+    public PayDebtRequest(Long fromUserId, Long toUserId, double amount) {
+        this.fromUserId = fromUserId;
+        this.toUserId = toUserId;
+        this.amount = amount;
+    }
+
+    public PayDebtRequest() {
+    }
+
+    public Long getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(Long fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    public Long getToUserId() {
+        return toUserId;
+    }
+
+    public void setToUserId(Long toUserId) {
+        this.toUserId = toUserId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
