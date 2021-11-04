97c82938542caabbec4063b3e1d6b6f61cd6e2b5
tdallas <tdallas@itba.edu.ar>
2021-11-04 18:36:59 -0300

    feat: added logic to support new members (although disgusting ui)

--- wallendar-app/app/src/main/java/com/td/wallendar/addmembers/ui/AddMembersActivity.java
+++ wallendar-app/app/src/main/java/com/td/wallendar/addmembers/ui/AddMembersActivity.java
@@ -1,28 +1,79 @@
 package com.td.wallendar.addmembers.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.td.wallendar.AbstractActivity;
 import com.td.wallendar.R;
-import com.td.wallendar.addgroup.ui.AddGroupPresenter;
+import com.td.wallendar.addmembers.MembersAdapter;
 import com.td.wallendar.di.DependenciesContainer;
 import com.td.wallendar.di.DependenciesContainerLocator;
 import com.td.wallendar.repositories.interfaces.GroupsRepository;
 
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
 public class AddMembersActivity extends AbstractActivity implements AddMembersView {
     private AddMembersPresenter addMembersPresenter;
+
+    private TextInputLayout memberInputLayout;
+    private Button tickToAddMember;
+    private RecyclerView recyclerView;
+
+    private MembersAdapter membersAdapter;
+    private List<String> members = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_add_group);
+        setContentView(R.layout.activity_add_members);
 
+        setupAdapter();
         setupActionBar();
 
+        setupInput();
+        setupTick();
+        setupRecyclerView();
+
         createPresenter();
+    }
+
+    private void setupAdapter() {
+        membersAdapter = new MembersAdapter(members);
+    }
+
+    private void setupRecyclerView() {
+        recyclerView = findViewById(R.id.current_members_added);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(membersAdapter);
+    }
+
+    private void setupTick() {
+        // FIXME This is not a button :(
+        tickToAddMember = findViewById(R.id.add_member_button);
+        tickToAddMember.setOnClickListener(view -> {
+            final String currentMember = Objects.requireNonNull(memberInputLayout.getEditText()).getText().toString();
+            memberInputLayout.getEditText().getText().clear();
+            membersAdapter.addToDataset(currentMember);
+        });
+    }
+
+    private void setupInput() {
+        memberInputLayout = findViewById(R.id.member_layout);
     }
 
     private void createPresenter() {
@@ -43,7 +94,13 @@ public class AddMembersActivity extends AbstractActivity implements AddMembersVi
 
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_close_white);
-        actionBar.setTitle(R.string.add_group);
+        actionBar.setTitle(R.string.add_members);
     }
 
+    @Override
+    public void onMembersAdded(Set<Member> members) {
+        final Intent resultIntent = new Intent();
+        setResult(RESULT_OK, resultIntent);
+        finish();
+    }
 }
--- wallendar-app/app/src/main/java/com/td/wallendar/addmembers/ui/AddMembersView.java
+++ wallendar-app/app/src/main/java/com/td/wallendar/addmembers/ui/AddMembersView.java
@@ -1,4 +1,8 @@
 package com.td.wallendar.addmembers.ui;
 
+import java.lang.reflect.Member;
+import java.util.Set;
+
 public interface AddMembersView {
+    void onMembersAdded(final Set<Member> members);
 }
