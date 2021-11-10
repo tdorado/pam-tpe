60a64293ba936c958a42d204eac8ac839f1cf075
Tomás Dorado <tdorado@itba.edu.ar>
2021-11-10 10:55:52 -0300

    fix: force light mode (#102)

--- wallendar-app/app/src/main/java/com/td/wallendar/MainApplication.java
+++ wallendar-app/app/src/main/java/com/td/wallendar/MainApplication.java
@@ -2,11 +2,14 @@ package com.td.wallendar;
 
 import android.app.Application;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 public class MainApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 
 }
